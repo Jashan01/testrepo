91068c84b0271055bfd1bf72147a1982bef50a52
SurabhiMishra <72308754+SurabhiMishra@users.noreply.github.com>
2020-10-10 01:45:54 +0530

    Update Insertion sort.py

--- Insertion sort.py
+++ Insertion sort.py
@@ -1,1 +1,15 @@
-#to be written
+#List taken from the user and sorted using insertion sort.
+
+print("Enter the list you want to sort(with spaces)")
+l=list(map(int,input().split()))
+n=len(l)
+print("The list you entered: ",l)
+
+for i in range(1,n):
+    value = l[i]
+    pos=i
+    while pos>0 and value<l[pos-1]:
+        l[pos]=l[pos-1]
+        pos=pos-1
+    l[pos]=value
+print("The sorted list: ",l)
